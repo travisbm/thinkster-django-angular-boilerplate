097f774bcf71d220417a92a4b1400b3414d4f98d
Travis Montgomery <travisbm@gmail.com>
2016-08-23 10:22:28 -0500

    add authentication app

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'compressor',
+    'authentication',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -113,3 +114,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+# Use Account class in authentication for authentication
+AUTH_USER_MODEL = 'authentication.Account'
